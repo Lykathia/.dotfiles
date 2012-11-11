d094c5a084c44df33a1da17aa764ef7f418fe55d
Evan Lowry <freyr@edev1.(none)>
2012-11-10 22:31:43 -0400

    xmonad updates

--- xmonad.hs
+++ xmonad.hs
@@ -4,7 +4,8 @@ import System.Exit
 import XMonad
 
 import Data.Char (chr)
-import Data.List (isInfixOf)
+import Data.IORef
+import Control.Monad (join)
 
 import qualified Data.Map as M
 import qualified XMonad.StackSet as W
@@ -16,24 +17,33 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.UrgencyHook
 
+import XMonad.Layout.Fullscreen
 import XMonad.Layout.Gaps
+import XMonad.Layout.IM
 import XMonad.Layout.LayoutHints (layoutHints)
-import XMonad.Layout.Named
 import XMonad.Layout.NoBorders (noBorders, smartBorders, withBorder)
+import XMonad.Layout.PerWorkspace
+import XMonad.Layout.Renamed
 import XMonad.Layout.Tabbed
 
+import XMonad.Util.Cursor
 import XMonad.Util.Run (hPutStrLn, spawnPipe)
 
+data KeyboardLayout = Qwerty | Colemak deriving (Show)
+
 ---------------------------------------------------------------------
---  Dzen Bars / Trayer
+--  Bar Setup
 ---------------------------------------------------------------------
+
+-- Workspace Names
+myWorkspaces :: [WorkspaceId]
+myWorkspaces = ["1:main", "2:web", "3:chat", "4:code", "5:code"] ++ map show [6..8] ++ ["9:misc"]
 
 -- Dzen
-dzenCmd :: String
-dzenCmd = "dzen2 -ta 'l'"
-
-delim :: Char
-delim = chr 127
+workspaceDzenCmd :: String
+workspaceDzenCmd = "dzen2 -x '0' -y '884' -h '16' -ta 'l'"
+tempDzen1 = "dzen2 -x '0' -y '0' -w '1440' -xs '0' -ta 'l'"
+tempDzen2 = "dzen2 -x '0' -y '0' -w '1440' -xs '1' -ta 'r'"
 
 -- Formatting for dzen
 myDzenPP :: Handle -> PP
@@ -43,52 +53,16 @@ myDzenPP h = dzenPP
     ,   ppHidden            = dzenColor "#EEEEEE" "#444444" . pad
     ,   ppHiddenNoWindows   = dzenColor "#999999" "#333333" . pad
     ,   ppUrgent            = dzenColor "#333333" "#CCCC33" . pad
-    ,   ppSep               = "|"
-    ,   ppWsSep             = "|"
-    ,   ppTitle             = dzenColor "#66CC66" "#333333" . pad
+    ,   ppSep               = "^fg(" ++ "#339933" ++ ")|"
+    ,   ppWsSep             = ""
+    ,   ppTitle             = dzenColor "#66CC66" "#333333" . pad . titleText . dzenEscape
     ,   ppOrder             = \(ws:l:t:_) -> [l,ws,t]
     ,   ppLayout            = dzenColor "#EEEEEE" "#3939EE" . pad
-    ,   ppOutput            = (\x -> hPutStrLn h $ formatStatus x)
+    ,   ppOutput            = hPutStrLn h
     }
     where
-        formatStatus s = concat $ map filterDividers $ strSplit delim s
-
-mkSolidDivider :: String -> String -> Int -> String
-mkSolidDivider _ _ 0 = ""
-mkSolidDivider fg bg n = colorLead $ concat $ map mkRect $ map scaleSizes sizes
-  where
-    sizes = [0..(n-1)]
-    scaleSizes = (\x -> n*3 - x*3)
-    mkRect = (\x -> "^r(1x" ++ show (x) ++ ")")
-    colorLead rects = "^pa(;0)^fg(" ++ fg ++ ")^bg(" ++ bg ++ ")" ++ rects ++ "^pa(;2)"
-
-mkLineDivider :: String -> Int -> String
-mkLineDivider _ 0 = ""
-mkLineDivider color n = colorLine $ concat $ map mkRect $ map scaleSizes sizes
-  where
-    sizes = [0..(n-1)]
-    scaleSizes = (\x -> (n-1)*3 - x*3)
-    mkRect = (\x -> "^pa(;" ++ show x ++ ")^r(1x3)")
-    colorLine rects = "^fg(" ++ color ++ ")" ++ rects ++ "^pa(;2)"
-
-strSplit :: Char -> String -> [String]
-strSplit _ "" = []
-strSplit c str = case dropWhile (==c) str of
-    "" -> []
-    str' -> w : strSplit c str''
-        where (w, str'') = break (== c) str'
-
-filterDividers :: String -> String
-filterDividers str
-    | length str == 15 && ['|'] `isInfixOf` str = makeDivider $ strSplit '|' str
-    | otherwise = str
-    where
-        makeDivider (fg:bg:_)
-            | fg /= bg = mkSolidDivider fg bg 5
-            | otherwise = mkLineDivider "#666666" 5
-
--- Workspace Names
-myWorkspaces = ["1:main", "2:web", "3:chat", "4:code", "5:code"] ++ map show [6..8] ++ ["9:misc"]
+        titleText [] = "Desktop"
+        titleText x = (shorten 82 x)
 
 ---------------------------------------------------------------------
 --  Colours / Fonts / Appearance
@@ -99,6 +73,9 @@ barXFont            = "inconsolata:size=12"
 xftFont             = "xft: inconsolata-14"
 myFocusedBorder     = "#33cc33"
 myUnfocusedBorder   = "#333333"
+
+delimChar :: Char
+delimChar = chr 127
 
 myTabTheme = defaultTheme
     {   fontName            = barXFont
@@ -114,69 +91,67 @@ myTabTheme = defaultTheme
 --  Hooks
 ---------------------------------------------------------------------
 
+myManageHook :: ManageHook
 myManageHook = composeAll
     [ className =? "MPlayer"        --> doFloat
     , className =? "Gimp"           --> doFloat
     , className =? "steam"          --> doFloat <+> doShift "1:main"
-    , className =? "Xfce4-notifyd"  --> doF W.focusDown ]
+    , className =? "Xfce4-notifyd"  --> doF W.focusDown
+    ]
 
 ---------------------------------------------------------------------
 --  Layouts
 ---------------------------------------------------------------------
--- http://www.haskell.org/haskellwiki/Xmonad/Config_archive/Nnoell%27s_xmonad.hs
-
--- Hook
-{-myLayout = avoidStruts 
-    $ gaps [(U,16), (D,16), (L,0), (R,0)]
-    $ layoutHints 
-    $ smartBorders
-    $ allLayouts
-    where
-        allLayouts  = myTabs
-        --webLayouts  =
-        --codeLayouts = 
-        --chatLayouts = 
--}
 
 -- Layouts
 {-
-myTabs  = named "TS"    $ smartBorders $ tabbed shrinkText myTabTheme
+tiledTab    = renamed "A"  $ smartBorders $ ResizableTall 1 0.03 0.5 []
+chatTab     = renamed "C"  $ withIM (0.20) (Title "Buddy List") $ Mirror $ ResizableTall 1 0.03 0.5 []
 -}
+
+-- Hook
+{-
+myLayout = 
+    $ avoidStruts
+    $ onWorkspace
+    where
+        defaultLayout = 
+-}
+
 myLayout = avoidStruts (tiled ||| Mirror tiled ||| Full)
   where
-     -- default tiling algorithm partitions the screen into two panes
-     tiled   = Tall nmaster delta ratio
+    -- default tiling algorithm partitions the screen into two panes
+    tiled   = Tall nmaster delta ratio
  
-     -- The default number of windows in the master pane
-     nmaster = 1
+    -- The default number of windows in the master pane
+    nmaster = 1
  
-     -- Default proportion of screen occupied by master pane
-     ratio   = 1/2
+    -- Default proportion of screen occupied by master pane
+    ratio   = 1/2
  
-     -- Percent of screen to increment by when resizing panes
-     delta   = 3/100
+    -- Percent of screen to increment by when resizing panes
+    delta   = 3/100
 
 --------------------------------------------------------------------
 --  Keybindings
 ---------------------------------------------------------------------
 
---  Toggle Dzen
-toggleStrutsKey :: XConfig t -> (KeyMask, KeySym)
-toggleStrutsKey XConfig {XMonad.modMask = mMask} = (mMask, xK_b)
-
 --  Keyboard Bindings
 myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
  
+    -----------------------------------------------------------------
+    --  Programs
+    -----------------------------------------------------------------
     -- launch a terminal
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
  
     -- launch dmenu
     , ((modm,               xK_p     ), spawn "exe=`dmenu_path | dmenu` && eval \"exec $exe\"")
  
-    -- launch gmrun
-    , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
- 
+    -----------------------------------------------------------------
+    --  Window Control
+    -----------------------------------------------------------------
     -- close focused window
     , ((modm .|. shiftMask, xK_c     ), kill)
  
@@ -191,12 +166,11 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
  
     -- Move focus to the next window
     , ((modm,               xK_Tab   ), windows W.focusDown)
- 
-    -- Move focus to the next window
     , ((modm,               xK_j     ), windows W.focusDown)
  
     -- Move focus to the previous window
-    , ((modm,               xK_k     ), windows W.focusUp  )
+    , ((modm,               xK_k     ), windows W.focusUp)
+    , ((modm .|. shiftMask, xK_Tab   ), windows W.focusUp)
  
     -- Move focus to the master window
     , ((modm,               xK_m     ), windows W.focusMaster  )
@@ -225,6 +199,15 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     -- Deincrement the number of windows in the master area
     , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
  
+    -----------------------------------------------------------------
+    --  Misc
+    -----------------------------------------------------------------
+    -- Colemak
+    , ((modm,               xK_a     ), spawn "setxkbmap us -variant colemak")
+
+    -- Qwerty
+    , ((modm,               xK_z     ), spawn "setxkbmap us")
+
     -- Quit xmonad
     , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
  
@@ -233,21 +216,15 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     ]
     ++
  
-    --
-    -- mod-[1..9], Switch to workspace N
-    --
     -- mod-[1..9], Switch to workspace N
     -- mod-shift-[1..9], Move client to workspace N
-    --
     [((m .|. modm, k), windows $ f i)
         | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
     ++
  
-    --
     -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
     -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
-    --
     [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
         | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
@@ -258,8 +235,9 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 
 myStartupHook :: X ()
 myStartupHook = do
-    spawn "trayer --edge top --align right --SetDockType true --SetPartialStrut false --expand true --width 10 --transparent true --alpha 0 --tint 0x0c0c0b --height 18 --monitor 1"
+    spawn "trayer --edge bottom --align right --SetDockType true --SetPartialStrut false --expand true --width 10 --transparent true --alpha 0 --tint 0x0c0c0b --height 16 --monitor 1"
     spawn "nm-applet"
+    setDefaultCursor xC_left_ptr 
 
 ---------------------------------------------------------------------
 --  XMonad Main
@@ -267,18 +245,22 @@ myStartupHook = do
 
 main :: IO ()
 main = do
-    h <- spawnPipe dzenCmd
+    -- Bars
+    workspaceBar    <- spawnPipe workspaceDzenCmd
+    topLeftDzenBar  <- spawnPipe tempDzen1 
+    topRightDzenBar <- spawnPipe tempDzen2
+
     xmonad $ withUrgencyHook NoUrgencyHook $ defaultConfig
-        {   terminal                = "urxvt"
+        {   terminal                = "urxvtc"
         ,   workspaces              = myWorkspaces
 
         -- Hooks / Layouts
         ,   layoutHook              = myLayout
         ,   manageHook              = (doF W.swapDown) <+> myManageHook
-        --,   handleEventHook         = myEventHook
+        ,   handleEventHook         = fullscreenEventHook
         ,   startupHook             = myStartupHook
         ,   logHook                 = do
-                dynamicLogWithPP $ myDzenPP h
+                dynamicLogWithPP $ myDzenPP workspaceBar
                 fadeInactiveLogHook 1.0
 
         -- Graphical stuff
