1b7736cf2defc95f135d4623f0f2a8e6ad799174
Lykathia <lowry.e@gmail.com>
2013-02-24 14:33:24 -0400

    starting install script

--- install.sh
+++ install.sh
@@ -6,3 +6,13 @@
 # Optional
 # if mpd : ncmpcpp
 # htop
+
+
+#if not $HOME/xmonad
+mkdir $HOME/.xmonad
+ln -s $HOME/.xmonad/xmonad.hs $HOME/.dotfiles/xmonad.hs
+
+ln -s $HOME/.vimrc $HOME/.dotfiles/vim/.vimrc
+ln -s $HOME/.xinit $HOME/.dotfiles/xorg/xinitrc
+ln -s $HOME/.Xresources $HOME/.dotfiles/xorg/Xresources
+ln -s $HOME/.zshrc $HOME/.dotfiles/zshrc
